566c0a1e72018202c03fb098a957140e4913a02d
Glenn Alcala <glenndelyp@gmail.com>
2026-05-05 22:08:30 +0800

    change database

--- src/pages/lib/db.js
+++ src/pages/lib/db.js
@@ -1,5 +1,6 @@
-// lib/db.js
-// Reuses the same @neondatabase/serverless package already in your project
-import { neon } from '@neondatabase/serverless'
 
-export const sql = neon(process.env.DATABASE_URL)
+import postgres from 'postgres'
+
+const sql = postgres(process.env.DATABASE_URL)
+
+export { sql }
